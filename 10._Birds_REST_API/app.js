4eb1a92a595250ad872fa786da8a677f63485445
Thom <92270696+Thom719c@users.noreply.github.com>
2023-02-24 09:27:38 +0100

    Updated the code for Bird API

--- 10._Birds_REST_API/app.js
+++ 10._Birds_REST_API/app.js
@@ -25,34 +25,32 @@ app.post("/birds", (req, res) => {
 	const bird = req.body;
 	bird.id = ++currentId;
 	birds.push(bird);
-	res.send({ data: req.body });
+	res.send({ data: bird });
 });
 
 // PATCH	/birds/<id>     Updates an existing bird by id.
 app.patch("/birds/:id", (req, res) => {
-	const foundBird = birds.find(bird => bird.id === Number(req.params.id));
-	const foundBirdIndex = birds.indexOf(foundBird, 0);
-
-	if (foundBirdIndex !== -1) {
-		const updateBirdWith = req.body;
-		const updatedBird = { ...foundBird, ...updateBirdWith, id: foundBird.id };
+	const foundBirdIndex = birds.findIndex(bird => bird.id === Number(req.params.id));
+	
+	if (foundBirdIndex === -1) {
+		res.status(404).send({ message: `No bird with id: ${req.params.id}` });
+	} else {
+		const foundBird = birds[foundBirdIndex];
+		const updatedBird = { ...foundBird, ...req.body, id: foundBird.id };
 		birds[foundBirdIndex] = updatedBird;
 
 		res.send({ data: updatedBird });
-	} else {
-		res.status(404).send({ message: `No bird with id: ${req.params.id}` });
 	}
 });
 
 // DELETE	/birds/<id>     Deletes an existing bird by id.
 app.delete("/birds/:id", (req, res) => {
 	const foundBird = birds.findIndex(bird => bird.id === Number(req.params.id));
-	if (foundBird !== -1) {
-		birds.splice(foundBird, 1);
-
-		res.status(200).json({ message: 'Deleted successfully!' });
+	if (foundBird === -1) {
+		res.status(404).json({ message: `No bird with id: ${req.params.id}` });
 	} else {
-		res.status(400).json({ message: `No bird with id: ${req.params.id}` });
+		const deletedbird = birds.splice(foundBird, 1)[0];
+		res.status(200).json({ data: deletedbird, message: 'Deleted successfully!' });
 	}
 });
 
