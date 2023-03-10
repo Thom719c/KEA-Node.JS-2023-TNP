ac33700af5a6d3c05a887b9b8cfead73ee13d67c
Thom <92270696+Thom719c@users.noreply.github.com>
2023-03-10 08:23:19 +0100

    Plain_HTML_Server updated

---
+++ 03._Plain_HTML_Server/public/museumGuards/barracks.js
@@ -0,0 +1,19 @@
+const guards = [
+    "Hank the tank",
+    "Thomas the tank engine",
+    "CEPUS"
+]
+
+/* export function getGuard() {
+    return guards;
+} */
+
+export function addGuard(guard) {
+    guards.push(guard);
+    return guards;
+}
+
+/* export default {
+    guards,
+    addGuard
+}; */
---
+++ 03._Plain_HTML_Server/public/museumGuards/guards.js
@@ -0,0 +1,6 @@
+// import { guards, addGuard } from "./barracks.js"
+import { addGuard } from "./barracks.js"
+
+// console.log(guards);
+
+console.log(addGuard("new guard"));
