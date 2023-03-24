4401ca5428a8d871da7d3320821bf9484d9006ee
Thom <92270696+Thom719c@users.noreply.github.com>
2023-03-24 09:29:02 +0100

    Created a dropwdown for the IRL Quests page that allows us to choose an acitivty

--- 04._Upper/public/pages/IRLQuests/IRLQuests.js
+++ 04._Upper/public/pages/IRLQuests/IRLQuests.js
@@ -1,10 +1,21 @@
 // url = https://www.boredapi.com/api/activity/
 
 /* Task fetch from boredapi and console log it */
-fetch("https://www.boredapi.com/api/activity/")
-    .then(response => response.json())
-    .then(res => {
-        console.log(res);
-        /* Task make the activity visible in the quest header tag */
-        document.getElementById("quest").innerText = res.activity + ".";
-    });
+function fetchQuest(activityQueryString = "") {
+    fetch("https://www.boredapi.com/api/activity" + activityQueryString)
+        .then(response => response.json())
+        .then(res => {
+            console.log(res);
+            /* Task make the activity visible in the quest header tag */
+            document.getElementById("quest").innerText = res.activity + ".";
+        });
+}
+fetchQuest();
+
+// const getNewQuest = (type) => {
+function getNewQuest() {
+    const dropdown = document.getElementById("activity-dropdown");
+    fetchQuest(`?type=${dropdown.value}`);
+}
+
+document.getElementById("fetch-new-quest-btn").addEventListener("click", getNewQuest);
